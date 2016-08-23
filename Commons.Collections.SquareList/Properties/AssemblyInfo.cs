136e04a39c7548684edfabff996d79e2ee3a65a6
Rafael Teixeira <monoman@gmail.com>
2016-08-22 23:34:55 -0300

    Try to pack a Nuget

--- Commons.Collections.SquareList/Properties/AssemblyInfo.cs
+++ Commons.Collections.SquareList/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Commons.Collections.SquareList")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Managed Commons library for SquareList specialized collection.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Managed Commons")]
 [assembly: AssemblyProduct("Commons.Collections.SquareList")]
